90adec517b1b0a1f8b350f13d7cd14bad2d8185a
Hui.Liu <31722995+Nuclear-Liu@users.noreply.github.com>
2021-11-29 09:10:24 +0800

    TDD

--- tdd/junit4/src/test/java/org/hui/tdd/junit4/demo/DefaultControllerTest.java
+++ tdd/junit4/src/test/java/org/hui/tdd/junit4/demo/DefaultControllerTest.java
@@ -19,29 +19,58 @@ public class DefaultControllerTest {
         controller = new DefaultController();
         request = new SampleRequest();
         handler = new SampleHandler();
+        controller.addHandler(request, handler);
     }
     @Test
     public void testAddHandler() {
-        controller.addHandler(request, handler);
         RequestHandler handler2 = controller.getHandler(request);
         assertSame("Handler we set in controller should be the same handler we get", handler2, handler);
     }
     @Test
     public void testProcessRequest() {
-        controller.addHandler(request, handler);
         Response response = controller.processRequest(request);
         assertNotNull("Must not return a null response", response);
         assertEquals("Response should be of type SampleResponse", new SampleResponse(), response);
     }
     @Test
-    public void testMethod() {
-        throw new RuntimeException("implement me");
+    public void testProcessRequestAnswersResponse() {
+        SampleRequest request = new SampleRequest("testError");
+        SampleExceptionHandler handler = new SampleExceptionHandler();
+        controller.addHandler(request, handler);
+        Response response = controller.processRequest(request);
+
+        assertNotNull("Must not return a nul response", response);
+        assertEquals(ErrorResponse.class, response.getClass());
+    }
+    @Test(expected = RuntimeException.class)
+    public void testGetHandlerNotDefined() {
+        Request request = new SampleRequest("testNotDefined");
+
+        // The following line is supposed to throw a RuntimeException
+        controller.getHandler(request);
+    }
+    @Test(expected = RuntimeException.class)
+    public void testAddRequestDuplicateName() {
+        Request request = new SampleRequest();
+        RequestHandler handler = new SampleHandler();
+
+        // The following line is supposed to throw a RuntimeException
+        controller.addHandler(request, handler);
     }
 
     private class SampleRequest implements Request {
+        private static final String DEFAULT_NAME = "Test";
+        private String name;
+
+        public SampleRequest(String name) {
+            this.name = name;
+        }
+        public SampleRequest() {
+            this.name = DEFAULT_NAME;
+        }
         @Override
         public String getName() {
-            return "Test";
+            return name;
         }
     }
 
@@ -70,6 +99,13 @@ public class DefaultControllerTest {
         @Override
         public Response process(Request request) throws Exception {
             return new SampleResponse();
+        }
+    }
+
+    private class SampleExceptionHandler implements RequestHandler {
+        @Override
+        public Response process(Request request) throws Exception {
+            throw  new Exception("error processing request");
         }
     }
 }
