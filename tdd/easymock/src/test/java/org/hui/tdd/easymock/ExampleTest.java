258060faed97d467a4cf3948b9c12fad36721d25
Hui.Liu <31722995+Nuclear-Liu@users.noreply.github.com>
2021-11-30 13:05:42 +0800

    TDD

---
+++ tdd/easymock/src/test/java/org/hui/tdd/easymock/ExampleTest.java
@@ -0,0 +1,31 @@
+package org.hui.tdd.easymock;
+
+import org.easymock.EasyMockRule;
+import org.easymock.EasyMockSupport;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Hui.Liu
+ * @since 2021-11-30 12:48
+ */
+public class ExampleTest extends EasyMockSupport {
+    @Rule
+    public EasyMockRule rule = new EasyMockRule(this);
+
+    @Mock
+    private Collaborator collaborator; // 1
+
+    @TestSubject
+    private ClassTested classUnderTest = new ClassTested(); // 2
+
+    @Test
+    public void addDocument() {
+        collaborator.documentAdded("New Document"); // 3
+        replayAll(); // 4
+        classUnderTest.addDocument("New Document", "content"); // 5
+        verifyAll(); // 6
+    }
+}
