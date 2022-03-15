9a0768354504ddaaa1e444538c0c5c3a16dbc04b
Hui.Liu <31722995+Nuclear-Liu@users.noreply.github.com>
2022-03-15 14:55:25 +0800

    docs(notice): reflect 算法
    software notice
    
    [skip ci]

---
+++ dsaa/src/main/java/org/hui/dsaa/nowcoder/medium/hj24/Main.java
@@ -0,0 +1,11 @@
+package org.hui.dsaa.nowcoder.medium.hj24;
+
+/**
+ * @author Hui.Liu
+ * @since 2022-03-15 14:08
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
