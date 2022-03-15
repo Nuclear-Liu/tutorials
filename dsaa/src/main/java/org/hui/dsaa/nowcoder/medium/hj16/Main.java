49bd325c1dab192ec22243b2c8f3845437571576
Hui.Liu <31722995+Nuclear-Liu@users.noreply.github.com>
2022-03-15 10:05:36 +0800

    docs(notice): reflect 算法
    software notice
    
    [skip ci]

--- dsaa/src/main/java/org/hui/dsaa/nowcoder/medium/hj16/Main.java
+++ dsaa/src/main/java/org/hui/dsaa/nowcoder/medium/hj16/Main.java
@@ -17,11 +17,15 @@ public class Main {
             int[][] prices = new int[m + 1][3];
             int[][] weights = new int[m + 1][3];
             for (int i = 1; i <= m; i++) {
+
                 int v = sc.nextInt(); // price
                 int p = sc.nextInt(); // weight
                 int q = sc.nextInt(); // q = 0 # 主件 q > 1 # 附件 所属 主件编号
+
                 v /= 10; // price
+
                 p = p * v; // weight
+
                 if (q == 0) {
                     // 主件
                     prices[i][0] = v;
@@ -37,20 +41,25 @@ public class Main {
                 }
                 sc.nextLine();
             }
+
             int[][] dp = new int[m + 1][money + 1];
+
             for (int i = 1; i <= m; i++) {
                 for(int j = 1; j <= money; j++) {
-                    int a = prices[i][0];
-                    int b = weights[i][0];
-                    int c = prices[i][1];
-                    int d = weights[i][1];
-                    int e = prices[i][2];
-                    int f = weights[i][2];
+                    int mainP = prices[i][0];
+                    int mainW = weights[i][0];
+                    int sub1P = prices[i][1];
+                    int sub1W = weights[i][1];
+                    int sub2P = prices[i][2];
+                    int sub2W = weights[i][2];
 
-                    dp[i][j] = j - a >= 0 ? Math.max(dp[i - 1][j], dp[i - 1][j - a] + b) : dp[i - 1][j];
-                    dp[i][j] = j - a - c >= 0 ? Math.max(dp[i][j], dp[i - 1][j - a - c] + b + d):dp[i][j];
-                    dp[i][j] = j - a - e >= 0 ? Math.max(dp[i][j], dp[i - 1][j - a - e] + b + f):dp[i][j];
-                    dp[i][j] = j - a - c - e >= 0 ? Math.max(dp[i][j], dp[i - 1][j - a - c - e] + b +d + f):dp[i][j];
+                    dp[i][j] = j - mainP >= 0 ? Math.max(dp[i - 1][j], dp[i - 1][j - mainP] + mainW) : dp[i - 1][j];
+
+                    dp[i][j] = j - mainP - sub1P >= 0 ? Math.max(dp[i][j], dp[i - 1][j - mainP - sub1P] + mainW + sub1W):dp[i][j];
+
+                    dp[i][j] = j - mainP - sub2P >= 0 ? Math.max(dp[i][j], dp[i - 1][j - mainP - sub2P] + mainW + sub2W):dp[i][j];
+
+                    dp[i][j] = j - mainP - sub1P - sub2P >= 0 ? Math.max(dp[i][j], dp[i - 1][j - mainP - sub1P - sub2P] + mainW +sub1W + sub2W):dp[i][j];
                 }
             }
             System.out.println(dp[m][money] * 10);
