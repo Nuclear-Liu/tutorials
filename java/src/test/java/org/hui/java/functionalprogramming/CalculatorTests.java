c6af32200f0a79ce160b53a34c764df64a6d1378
Hui.Liu <Hui@Hui.com>
2022-06-27 20:59:07 +0800

    java doc

--- java/src/test/java/org/hui/java/functionalprogramming/CalculatorTests.java
+++ java/src/test/java/org/hui/java/functionalprogramming/CalculatorTests.java
@@ -1,8 +1,8 @@
 package org.hui.java.functionalprogramming;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
 import org.hui.java.functionalprogramming.Calculator.IntegerMath;
+import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class CalculatorTests {
@@ -10,8 +10,45 @@ public class CalculatorTests {
     public void testOperateBinary() {
         Calculator myApp = new Calculator();
         IntegerMath addition = (a, b) -> a + b;
-        IntegerMath subtraction = (a, b) ->  a - b;
+        IntegerMath subtraction = (a, b) -> a - b;
         log.info("40 + 2 = {}", myApp.operateBinary(40, 2, addition));
         log.info("20 - 10 = {}", myApp.operateBinary(20, 10, subtraction));
     }
+
+    @Test
+    public void test() throws InterruptedException {
+        String context = "context";
+        // context = "Hello";
+        Runnable runner = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(context);
+            }
+        };
+        Thread t = new Thread(runner);
+        t.start();
+        t.join();
+    }
+
+    @Test
+    public void testFuncationInterface() {
+        C c = () -> "GetC";
+
+        System.out.println(c.getC());
+        System.out.println(c.getB());
+
+        D d = () -> "GetD";
+
+        System.out.println(d.getB());
+    }
+
+    @Test
+    public void testFin() {
+        FF f = () -> {
+            return () -> "return A";
+            // return new Object();
+        };
+    }
+
+
 }
---
+++ java/src/test/java/org/hui/java/functionalprogramming/FF.java
@@ -0,0 +1,8 @@
+package org.hui.java.functionalprogramming;
+
+import org.hui.java.functionalprogramming.A;
+import org.hui.java.functionalprogramming.D;
+
+public interface FF {
+    D getA();
+}
