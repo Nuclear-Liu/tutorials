14743b4e909bc97b77c334795a400db66e589ed2
hui <liu@hui.org>
2022-12-07 11:14:46 +0800

    websocket.

---
+++ websocket/websocket/src/main/java/org/hui/websocket/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package org.hui.websocket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * 首先注入 {@link ServerEndpointExporter} ，这个 Bean 会自动注册使用了 {@link javax.websocket.server.ServerEndpoint} 注解声明的 WebSocket Endpoint.
+ * <p>
+ * 如果使用独立的 servlet 容器，而不是使用 Spring Boot 内置容器，不需要注入 {@link ServerEndpointExporter} ，因为它需要独立容器提供管理.
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
---
+++ websocket/websocket/src/main/java/org/hui/websocket/config/WebSocketEndpoint.java
@@ -0,0 +1,32 @@
+package org.hui.websocket.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+
+@Component
+@ServerEndpoint(value = "/websocket/{userId}")
+public class WebSocketEndpoint {
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("userId") String userId) {
+        SessionPool.sessions.put(userId, session);
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String message) {
+        SessionPool.sendMessage(message);
+    }
+
+    @OnClose
+    public void onClose(Session session) throws IOException {
+        SessionPool.close(session.getId());
+        session.close();
+    }
+}
