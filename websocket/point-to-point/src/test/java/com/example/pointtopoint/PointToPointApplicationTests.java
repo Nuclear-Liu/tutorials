89f3f0d048608021d35967d88d0bdd7194d24142
hui <liu@hui.org>
2022-12-07 11:46:37 +0800

    websocket.

---
+++ websocket/point-to-point/src/test/java/com/example/pointtopoint/PointToPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pointtopoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointToPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
