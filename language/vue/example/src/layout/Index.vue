55df3af9ca7c7845f5dc87001c20761591a8db5e
Hui.Liu <mexn-0808@outlook.com>
2023-06-17 17:48:46 +0800

    dynamic icon in action.

--- language/vue/example/src/layout/Index.vue
+++ language/vue/example/src/layout/Index.vue
@@ -1,38 +1,40 @@
 <script setup lang="ts">
 import Header from '@/layout/header/Header.vue'
 import MenuBar from "@/layout/menu/MenuBar.vue";
+import Tabs from "@/layout/tabs/Tabs.vue";
 </script>
 
 <template>
-  <el-container class="layout">
-    <el-aside class="aside" width="auto">
-      <MenuBar></MenuBar>
-    </el-aside>
     <el-container class="layout">
-      <el-header class="header" height="40px">
-        <Header></Header>
-      </el-header>
-      <el-main class="main">
-        <router-view></router-view>
-      </el-main>
+        <el-aside class="aside" width="auto">
+            <MenuBar></MenuBar>
+        </el-aside>
+        <el-container class="layout">
+            <el-header class="header" height="40px">
+                <Header></Header>
+            </el-header>
+            <el-main class="main">
+                <Tabs></Tabs>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
     </el-container>
-  </el-container>
 </template>
 
 <style lang="scss" scoped>
 .layout {
-  height: 100%;
+    height: 100%;
 
-  .aside {
-    background-color: #304156;
-  }
+    .aside {
+        background-color: #304156;
+    }
 
-  .header {
-    border-bottom: 1px solid #e5e5e5;
-  }
+    .header {
+        border-bottom: 1px solid #e5e5e5;
+    }
 
-  .main {
-    //background-color: darkcyan;
-  }
+    .main {
+        //background-color: darkcyan;
+    }
 }
 </style>
--- language/vue/example/src/store/index.ts
+++ language/vue/example/src/store/index.ts
@@ -1,17 +1,21 @@
 import {InjectionKey} from "vue";
 // @ts-ignore
 import {createStore, Store, useStore as baseUseStore} from "vuex";
+import type {ITab} from '@/store/type/index.ts'
 
 export interface State {
     count: number,
-    collapse: boolean
+    collapse: boolean,
+    tabList: Array<ITab>
 }
 
 export const key: InjectionKey<Store<State>> = Symbol()
 
 export const store = createStore<State>({
     state: {
-        count: 0, collapse: false
+        count: 0,
+        collapse: false,
+        tabList: []
     },
     mutations: {
         setCount(state: State, count: number) {
@@ -19,6 +23,13 @@ export const store = createStore<State>({
         },
         setCollapse: (state: State, collapse: boolean) => {
             state.collapse = collapse;
+        },
+        addTab: (state: State, tab: ITab) => {
+            // 判断是否已经存在
+            if (state.tabList.some(item => item.path === tab.path)) {
+                return;
+            }
+            state.tabList.push(tab);
         }
     },
     getters: {
@@ -27,6 +38,9 @@ export const store = createStore<State>({
         },
         getCollapse: (state: State) => {
             return state.collapse;
+        },
+        getTabList:(state: State) => {
+            return state.tabList;
         }
     }
 })
