d8cbe40d8b0296e0ea284c15ab4bf1bfa958f846
hui.liu <liu_hui@hui.org>
2023-06-19 07:35:20 +0800

    vue in action: refresh quest.

--- language/vue/example/src/store/index.ts
+++ language/vue/example/src/store/index.ts
@@ -1,49 +1,17 @@
 import {InjectionKey} from "vue";
 // @ts-ignore
 import {createStore, Store, useStore as baseUseStore} from "vuex";
-import type {ITab} from '@/store/type/index.ts'
+import {TabsState} from "@/store/modules/tabs/state.ts";
 
-export interface State {
-    count: number,
-    collapse: boolean,
-    tabList: Array<ITab>
+export interface RootState {
+    tabState:TabsState
 }
 
-export const key: InjectionKey<Store<State>> = Symbol()
+export const key: InjectionKey<Store<RootState>> = Symbol()
 
-export const store = createStore<State>({
-    state: {
-        count: 0,
-        collapse: false,
-        tabList: []
-    },
-    mutations: {
-        setCount(state: State, count: number) {
-            state.count = count;
-        },
-        setCollapse: (state: State, collapse: boolean) => {
-            state.collapse = collapse;
-        },
-        addTab: (state: State, tab: ITab) => {
-            // 判断是否已经存在
-            if (state.tabList.some(item => item.path === tab.path)) {
-                return;
-            }
-            state.tabList.push(tab);
-        }
-    },
-    getters: {
-        getCount(state: State) {
-            return state.count;
-        },
-        getCollapse: (state: State) => {
-            return state.collapse;
-        },
-        getTabList:(state: State) => {
-            return state.tabList;
-        }
-    }
-})
+export const store = createStore<RootState>({
+    modules
+}) as CommonStore
 
 export function useStore() {
     return baseUseStore(key)
