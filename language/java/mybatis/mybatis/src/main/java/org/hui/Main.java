1f21e56b889a8af55430e218f4f339ad34a1efec
hui.liu <liu_hui@hui.org>
2023-08-16 07:32:20 +0800

    mybatis in action.

--- language/java/mybatis/mybatis/src/main/java/org/hui/Main.java
+++ language/java/mybatis/mybatis/src/main/java/org/hui/Main.java
@@ -4,9 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        org.apache.ibatis.logging.LogFactory.useLog4JLogging();
+//        org.apache.ibatis.logging.LogFactory.useLog4JLogging();
         System.out.println("Hello world!");
         LOGGER.info("hello world");
     }
