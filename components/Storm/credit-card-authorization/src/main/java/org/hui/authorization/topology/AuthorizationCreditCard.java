a026fc9ff85ec6fc15a4328da374bd27733f11d9
Hui.Liu <mexn-0808@outlook.com>
2023-11-23 10:10:48 +0800

    storm in action.

--- components/Storm/credit-card-authorization/src/main/java/org/hui/authorization/topology/AuthorizationCreditCard.java
+++ components/Storm/credit-card-authorization/src/main/java/org/hui/authorization/topology/AuthorizationCreditCard.java
@@ -1,9 +1,10 @@
 package org.hui.authorization.topology;
 
+import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
-import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.FailedException;
 import org.apache.storm.topology.OutputFieldsDeclarer;
-import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
@@ -13,31 +14,38 @@ import org.hui.authorization.services.OrderDao;
 
 import java.util.Map;
 
-public class AuthorizationCreditCard extends BaseBasicBolt {
+public class AuthorizationCreditCard extends BaseRichBolt {
     private Map stormConf;
     private TopologyContext context;
+    private OutputCollector collector;
 
     private AuthorizationService authorizationService;
     private OrderDao orderDao;
 
     @Override
-    public void prepare(Map stormConf, TopologyContext context) {
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
         this.stormConf = stormConf;
         this.context = context;
+        this.collector = collector;
         orderDao = new OrderDao();
         authorizationService = new AuthorizationService();
     }
 
     @Override
-    public void execute(Tuple input, BasicOutputCollector collector) {
+    public void execute(Tuple input) {
         Order order = (Order) input.getValueByField("order");
-        boolean isAuthorized = authorizationService.authorize(order);
-        if (isAuthorized) {
-            orderDao.updateStatusToReadyToShip(order);
-        } else {
-            orderDao.updateStatusToDenied(order);
+        try {
+            boolean isAuthorized = authorizationService.authorize(order);
+            if (isAuthorized) {
+                orderDao.updateStatusToReadyToShip(order);
+            } else {
+                orderDao.updateStatusToDenied(order);
+            }
+            collector.emit(input, new Values(order));
+            collector.ack(input);
+        } catch (FailedException e) {
+            collector.fail(input);
         }
-        collector.emit(new Values(order));
     }
 
     @Override
