1fee39bebef47aec870baa7d001e42dafa091c11
Hui.Liu <mexn-0808@outlook.com>
2024-02-02 17:24:44 +0800

    microservices in action.

---
+++ components/spring-security/provider-h2/src/main/java/org/hui/security/provider/h2/controller/UserController.java
@@ -0,0 +1,25 @@
+package org.hui.security.provider.h2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "welcome";
+    }
+    @GetMapping("/welcome")
+    public String welcomePage() {
+        return "welcome";
+    }
+    @GetMapping ("/authenticated")
+    public String AuthenticatedPage() {
+        return "authenticated";
+    }
+    @GetMapping ("/logout")
+    public String logoutPage() {
+        return "redirect:/welcome";
+    }
+}
